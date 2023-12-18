157ba01766ca200570fbbac2d4d26b5fd7805d78
fujimoto@eyemovic.com <fujimoto@eyemovic.com>
2023-12-18 13:20:43 +0900

    csvからグラフ描画

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,56 +1,59 @@
-var ctx = document.getElementById("chart");
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: [
-            '1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
-        datasets: [
-            {
-                label: '予測降水量',
-                data: [10, 0, 0, 0, 0, 0, 4, 2, 0, 0, 0, 0],
-                borderColor: "rgba(255,0,0,1)",
-                backgroundColor: "rgba(0,0,0,0)",
-                yAxisID: 'rainfallYAxis'
+let ctx = document.getElementById("chart");
+let csv = new XMLHttpRequest();
+
+csv.open("GET", "../csv/result.csv", true);
+csv.send();
+csv.onload = function() {
+    if (csv.status === 200) {
+        let csvArray = [];
+
+        const lines = csv.responseText.split('\n');
+        const dataArray = lines.map(line => line.split(','));
+
+        // 日付と値を別々の配列に格納
+        const dates = dataArray.map(item => item[0]);
+        const values = dataArray.map(item => parseInt(item[1], 10));
+
+        // 結果を出力
+        console.log("日付配列:", dates);
+        console.log("値配列:", values);
+
+        let myLineChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: dates,
+                datasets: [
+                    {
+                        label: '予測来館者数',
+                        data: values,
+                        borderColor: "rgba(255,0,0,1)",
+                        backgroundColor: "rgba(0,0,0,0)",
+                        yAxisID: 'YAxis'
+                    },
+                ],
             },
-            {
-                label: '予測来館者数',
-                data: [150, 160, 230, 250, 200, 290, 180, 160, 180, 240, 220, 250],
-                borderColor: "rgba(0,0,255,1)",
-                backgroundColor: "rgba(0,0,0,0)",
-                yAxisID: 'visitorYAxis'
+            options: {
+                title: {
+                    display: true,
+                    text: '予測来館者数'
+                },
+                scales: {
+                    yAxes: [{
+                        id: 'YAxis',
+                        position: 'left',
+                        ticks: {
+                            suggestedMax: 40,
+                            suggestedMin: 0,
+                            stepSize: 10,
+                            callback: function(value){
+                                return value + '人';
+                            }
+                        }
+                    }]
+                },
             }
-        ],
-    },
-    options: {
-        title: {
-            display: true,
-            text: '予測降水量と予測来館者数'
-        },
-        scales: {
-            yAxes: [{
-                id: 'rainfallYAxis', // 予測降水量用のyAxis
-                position: 'left', // 左側に配置
-                ticks: {
-                    suggestedMax: 40,
-                    suggestedMin: 0,
-                    stepSize: 10,
-                    callback: function(value, index, values){
-                        return value + 'mm'; // 降水量の単位を追加
-                    }
-                }
-            },
-            {
-                id: 'visitorYAxis', // 予測来館者数用のyAxis
-                position: 'right', // 右側に配置
-                ticks: {
-                    suggestedMax: 300, // 来館者数の最大値
-                    suggestedMin: 0, // 来館者数の最小値
-                    stepSize: 50,
-                    callback: function(value, index, values){
-                        return value + '人'; // 来館者数の単位を追加
-                    }
-                }
-            }]
-        },
+        });
+    } else {
+        console.error('リクエストに失敗しました');
     }
-});
+};
